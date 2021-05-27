1b14a66a045b79ccc1be366c5eb95dd83f981ac8
Kevin Castro <kevin.castro.chavez0@gmail.com>
2021-05-26 20:41:28 -0600

    created elements dynamically based on the images fetched and set their attbutes

--- infinite.js
+++ infinite.js
@@ -1,12 +1,45 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+
+let photosArray = [];
+
 const count = 10;
 const apiKey = "waPU92E_1DCl7KU3FR7dr4qVitXFzPV5NK4qshYtBj0";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+function displayPhotos() {
+  photosArray.forEach((photo) => {
+    const item = document.createElement("a");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+
+    const img = document.createElement("img");
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
+
 async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data);
+    photosArray = await response.json();
+    console.log(photosArray);
+
+    displayPhotos();
   } catch (error) {
     console.log(error.message);
   }
