fa02685edb0477089414ec97f15d96c06686212b
Kevin Castro <kevin.castro.chavez0@gmail.com>
2021-05-26 20:48:13 -0600

    implemented the infinite scroll functionality using the height of the device and document

--- infinite.js
+++ infinite.js
@@ -45,4 +45,13 @@ async function getPhotos() {
   }
 }
 
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >=
+    document.body.offsetHeight - 1000
+  ) {
+    getPhotos();
+  }
+});
+
 getPhotos();
