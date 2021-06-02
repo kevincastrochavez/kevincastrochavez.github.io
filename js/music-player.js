ded5a3b94017dd0bbc52e427abd9ea45608d78ac
Kevin Castro <kevin.castro.chavez0@gmail.com>
2021-06-01 18:47:34 -0600

    started functionality of progressbar. Created functions to change the width on the progressbar based on the time that has passed

--- js/music-player.js
+++ js/music-player.js
@@ -1,7 +1,10 @@
 const image = document.querySelector("img");
 const title = document.getElementById("title");
 const artist = document.getElementById("artist");
+
 const music = document.querySelector("audio");
+const progressContainer = document.getElementById("progress-container");
+const progress = document.getElementById("progress");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
@@ -76,5 +79,15 @@ function nextSong() {
 
 loadSong(songs[songIndex]);
 
+function updateProgressBar(e) {
+  if (isPlaying) {
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+  }
+}
+
 prevBtn.addEventListener("click", prevSong);
 nextBtn.addEventListener("click", nextSong);
+
+music.addEventListener("timeupdate", updateProgressBar);
