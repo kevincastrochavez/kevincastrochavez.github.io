18ecb85475273a4f793fd5b87e9afcc9545d9a99
Kevin Castro <kevin.castro.chavez0@gmail.com>
2021-06-01 18:36:22 -0600

    built functionality for the previous and next song

--- js/music-player.js
+++ js/music-player.js
@@ -1,7 +1,33 @@
+const image = document.querySelector("img");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
 const music = document.querySelector("audio");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
+
+const songs = [
+  {
+    name: "song1",
+    displayName: "Electric Chill Machine",
+    artist: "Kevin Design",
+  },
+  {
+    name: "song2",
+    displayName: "Seven Nation Army",
+    artist: "Kevin Design",
+  },
+  {
+    name: "song3",
+    displayName: "Goodnight, Disco Queen",
+    artist: "Kevin Design",
+  },
+  {
+    name: "metric1",
+    displayName: "Front Row (Remix)",
+    artist: "Kevin Design",
+  },
+];
 
 let isPlaying = false;
 
@@ -20,3 +46,35 @@ function pauseSong() {
 }
 
 playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
+
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `music/${song.name}.mp3`;
+  image.src = `images/${song.name}.jpg`;
+}
+
+let songIndex = 0;
+
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+function nextSong() {
+  songIndex++;
+  if (songIndex > song.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+loadSong(songs[songIndex]);
+
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
