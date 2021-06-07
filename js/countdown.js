eebc282df432d84e4a701ce83239cc63c10b549d
Kevin Castro <kevin.castro.chavez0@gmail.com>
2021-06-07 15:46:40 -0600

    added function to get the value and date from the form inputs

--- js/countdown.js
+++ js/countdown.js
@@ -2,5 +2,18 @@ const inputContainer = document.getElementById("input-container");
 const countdownForm = document.getElementById("countdownForm");
 const dateEl = document.getElementById("date-picker");
 
+let countdownTitle = "";
+let countdownDate = "";
+
 const today = new Date().toISOString().split("T")[0];
 dateEl.setAttribute("min", today);
+
+function updateCountdown(e) {
+  e.preventDefault();
+  countdownTitle = e.srcElement[0].value;
+  countdownDate = e.srcElement[1].value;
+  console.log(countdownDate);
+  console.log(countdownTitle);
+}
+
+countdownForm.addEventListener("submit", updateCountdown);
