61c87ca26f113f159289af918ac9a1e426ae4e6d
Kevin Castro <kevin.castro.chavez0@gmail.com>
2021-06-16 11:55:14 -0600

    modified styles for the card, created individual containers for the text displayed

--- lesson9/js/iceChallenge.js
+++ lesson9/js/iceChallenge.js
@@ -16,15 +16,14 @@ fetch(requestURL)
       jsonObject.person.personal.name +
       " " +
       jsonObject.person.personal.last_name;
-    password.textContent =
-      "Password: " + jsonObject.person.online_info.password;
-    email.textContent = "Email: " + jsonObject.person.online_info.email;
-    eye.textContent = "Eye Color: " + jsonObject.person.personal.eye_color;
-    city.textContent = `City/Country: ${jsonObject.person.personal.city}/${jsonObject.person.personal.country}`;
-    if (jsonObject.person.marriage.married == true) {
-      children.textContent = "Children: " + jsonObject.person.marriage.children;
+    password.textContent = jsonObject.person.online_info.password;
+    email.textContent = jsonObject.person.online_info.email;
+    eye.textContent = jsonObject.person.personal.eye_color;
+    city.textContent = `${jsonObject.person.personal.city}/${jsonObject.person.personal.country}`;
+    if (jsonObject.person.marriage.married) {
+      children.textContent = jsonObject.person.marriage.children;
     } else {
-      children.textContent = "Children: 0";
+      children.textContent = "0";
     }
-    ip.textContent = `IP Address: ${jsonObject.person.online_info.ip_address}`;
+    ip.textContent = `${jsonObject.person.online_info.ip_address}`;
   });
