1d4030175a4f55a97b1f4031733bc3188ea2da01
Kevin Castro <kevin.castro.chavez0@gmail.com>
2021-06-23 18:31:40 -0600

    fetched the openweather api to get the 5-day forecast for the preston page, replaced the filler code from the HTML with the dynamic one gotten from the api

--- lesson10/js/script.js
+++ lesson10/js/script.js
@@ -1,33 +1,33 @@
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
 window.addEventListener("load", () => {
   const hamButton = document.querySelector(".ham");
   const mainNav = document.querySelector("#navigation");
   const dateFooter = document.querySelector(".date");
   const banner = document.querySelector(".banner");
-
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-
-  const days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
 
   const date = new Date();
   const day = date.getDate();
@@ -50,6 +50,9 @@ window.addEventListener("load", () => {
     },
     false
   );
+
+  fiveDayForecast();
+  apiCall();
 });
 
 const API_KEY = "90110c6c5296ba96a34270bcbcee731b";
@@ -63,30 +66,72 @@ const windChill = document.getElementById("windChill");
 
 const url = `https://api.openweathermap.org/data/2.5/weather?id=${CITY_ID}&appid=${API_KEY}&units=imperial`;
 
-// const apiCall = () => {
-//   fetch(url)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       console.log(data);
+const apiCall = () => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      currentCond.textContent = data.weather[0].main;
+      high.textContent = `${data.main.temp_max.toFixed(0)}°F`;
+      currentTemp.textContent = `${data.main.temp.toFixed(0)}°F`;
+      humidity.textContent = `${data.main.humidity}%`;
+      windSpeed.textContent = `${data.wind.speed.toFixed(0)}mph`;
 
-//       currentCond.textContent = data.weather[0].main;
-//       high.textContent = `${data.main.temp_max.toFixed(0)}°F`;
-//       currentTemp.textContent = `${data.main.temp.toFixed(0)}°F`;
-//       humidity.textContent = `${data.main.humidity}%`;
-//       windSpeed.textContent = `${data.wind.speed.toFixed(0)}mph`;
+      const temperatureSplit = Number(currentTemp.textContent.split("°")[0]);
+      const windSpeedSplit = Number(windSpeed.textContent.split("m")[0]);
 
-//       const temperatureSplit = Number(currentTemp.textContent.split("°")[0]);
-//       const windSpeedSplit = Number(windSpeed.textContent.split("m")[0]);
+      const windChillFactor = (
+        35.74 +
+        0.6215 * temperatureSplit -
+        35.75 * windSpeedSplit ** 0.16 +
+        0.4275 * temperatureSplit * windSpeedSplit ** 0.16
+      ).toFixed(0);
 
-//       const windChillFactor = (
-//         35.74 +
-//         0.6215 * temperatureSplit -
-//         35.75 * windSpeedSplit ** 0.16 +
-//         0.4275 * temperatureSplit * windSpeedSplit ** 0.16
-//       ).toFixed(0);
+      windChill.textContent = `${Number(windChillFactor)}°F`;
+    });
+};
 
-//       windChill.textContent = `${Number(windChillFactor)}°F`;
-//     });
-// };
+const fiveDayForecast = async () => {
+  const days = [
+    "Saturday",
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+  ];
+  const imagesArray = Array.from(document.querySelectorAll(".forecast-img"));
+  const daysArray = Array.from(document.querySelectorAll(".forecast-day"));
+  const temperaturesArray = Array.from(
+    document.querySelectorAll(".forecast-temperature")
+  );
 
-// apiCall();
+  const url = `https://api.openweathermap.org/data/2.5/forecast?id=${CITY_ID}&appid=${API_KEY}&units=imperial`;
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const filteredArray = data.list.filter(
+    (el) => el.dt_txt.split(" ")[1] == "18:00:00"
+  );
+
+  const dateAndTime = filteredArray.map((el) => el.dt_txt);
+  const getDay = dateAndTime.map((el) => getDayOfTheWeek(el));
+  const weekDays = getDay.map((day) => days[day]);
+  const iconsArray = filteredArray.map((icon) => icon.weather[0].icon);
+
+  daysArray.map((day, i) => (day.textContent = weekDays[i]));
+  imagesArray.map(
+    (image, i) =>
+      (image.src = `https://openweathermap.org/img/w/${iconsArray[i]}.png`)
+  );
+  temperaturesArray.map(
+    (temperature, i) =>
+      (temperature.textContent = `${filteredArray[i].main.temp.toFixed(0)}°F`)
+  );
+};
+
+const getDayOfTheWeek = (dateAndTime) => {
+  const date = new Date(dateAndTime);
+  const utcDate = new Date(date.toUTCString());
+  return utcDate.getUTCDay();
+};
--- lesson10/js/weatherApi.js
+++ lesson10/js/weatherApi.js
@@ -6,17 +6,26 @@ const icon = document.getElementById("icon");
 
 const url = `https://api.openweathermap.org/data/2.5/weather?id=${CITY_ID}&appid=${API_KEY}&units=imperial`;
 
-const apiCall = async () => {
+// const apiCall = async () => {
+//   const response = await fetch(url);
+//   const data = await response.json();
+
+//   currentTemp.textContent = data.main.temp;
+
+//   const imagesrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+//   const desc = data.weather[0].description;
+//   image.textContent = imagesrc;
+//   icon.setAttribute("src", imagesrc);
+//   icon.setAttribute("alt", desc);
+// };
+
+const fiveDayForecast = async () => {
+  const url = `https://api.openweathermap.org/data/2.5/forecast?id=${CITY_ID}&appid=${API_KEY}`;
   const response = await fetch(url);
   const data = await response.json();
 
-  currentTemp.textContent = data.main.temp;
-
-  const imagesrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-  const desc = data.weather[0].description;
-  image.textContent = imagesrc;
-  icon.setAttribute("src", imagesrc);
-  icon.setAttribute("alt", desc);
+  console.log(data);
 };
 
-apiCall();
+// apiCall();
+fiveDayForecast();
