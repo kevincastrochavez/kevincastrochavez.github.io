42598117b0b8e160db9b01c19a41eb1ecb608483
Kevin Castro <kevin.castro.chavez0@gmail.com>
2021-06-30 19:01:58 -0600

    modified code to get information from the API about Soda Springs and changed images

---
+++ lesson11/js/sodaSprings.js
@@ -0,0 +1,138 @@
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const dateFooter = document.querySelector(".date");
+const banner = document.querySelector(".banner");
+
+const date = new Date();
+const day = date.getDate();
+const month = date.getMonth();
+const year = date.getFullYear();
+const dayWeek = date.getDay();
+
+dateFooter.innerHTML = `${days[dayWeek]}, ${day} ${months[month]} ${year}`;
+
+if (dayWeek !== 5) {
+  banner.style.display = "none";
+} else {
+  banner.style.display = "flex";
+}
+
+window.addEventListener("load", () => {
+  const hamButton = document.querySelector(".ham");
+  const mainNav = document.querySelector("#navigation");
+
+  hamButton.addEventListener(
+    "click",
+    () => {
+      mainNav.classList.toggle("responsive");
+    },
+    false
+  );
+
+  fiveDayForecast();
+  apiCall();
+});
+
+const API_KEY = "90110c6c5296ba96a34270bcbcee731b";
+const CITY_ID = "5607916";
+const currentCond = document.getElementById("condition");
+const high = document.getElementById("high");
+const currentTemp = document.getElementById("temperature");
+const humidity = document.getElementById("humidity");
+const windSpeed = document.getElementById("windSpeed");
+const windChill = document.getElementById("windChill");
+
+const url = `https://api.openweathermap.org/data/2.5/weather?id=${CITY_ID}&appid=${API_KEY}&units=imperial`;
+
+const apiCall = () => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      currentCond.textContent = data.weather[0].main;
+      high.textContent = `${data.main.temp_max.toFixed(0)}°F`;
+      currentTemp.textContent = `${data.main.temp.toFixed(0)}°F`;
+      humidity.textContent = `${data.main.humidity}%`;
+      windSpeed.textContent = `${data.wind.speed.toFixed(0)}mph`;
+
+      const temperatureSplit = Number(currentTemp.textContent.split("°")[0]);
+      const windSpeedSplit = Number(windSpeed.textContent.split("m")[0]);
+
+      const windChillFactor = (
+        35.74 +
+        0.6215 * temperatureSplit -
+        35.75 * windSpeedSplit ** 0.16 +
+        0.4275 * temperatureSplit * windSpeedSplit ** 0.16
+      ).toFixed(0);
+
+      windChill.textContent = `${Number(windChillFactor)}°F`;
+    });
+};
+
+const fiveDayForecast = async () => {
+  const days = [
+    "Saturday",
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+  ];
+  const imagesArray = Array.from(document.querySelectorAll(".forecast-img"));
+  const daysArray = Array.from(document.querySelectorAll(".forecast-day"));
+  const temperaturesArray = Array.from(
+    document.querySelectorAll(".forecast-temperature")
+  );
+
+  const url = `https://api.openweathermap.org/data/2.5/forecast?id=${CITY_ID}&appid=${API_KEY}&units=imperial`;
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const filteredArray = data.list.filter(
+    (el) => el.dt_txt.split(" ")[1] == "18:00:00"
+  );
+
+  const dateAndTime = filteredArray.map((el) => el.dt_txt);
+  const getDay = dateAndTime.map((el) => getDayOfTheWeek(el));
+  const weekDays = getDay.map((day) => days[day]);
+  const iconsArray = filteredArray.map((icon) => icon.weather[0].icon);
+
+  daysArray.map((day, i) => (day.textContent = weekDays[i]));
+  imagesArray.map(
+    (image, i) =>
+      (image.src = `https://openweathermap.org/img/w/${iconsArray[i]}.png`)
+  );
+  temperaturesArray.map(
+    (temperature, i) =>
+      (temperature.textContent = `${filteredArray[i].main.temp.toFixed(0)}°F`)
+  );
+};
+
+const getDayOfTheWeek = (dateAndTime) => {
+  const date = new Date(dateAndTime);
+  const utcDate = new Date(date.toUTCString());
+  return utcDate.getUTCDay();
+};
