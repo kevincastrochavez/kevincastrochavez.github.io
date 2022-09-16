9da28eb971077380f0b76b26061629963e6e3e5d
Kevin <kevin.castro.chavez0@gmail.com>
2022-09-15 21:40:40 -0600

    added the forkify app

---
+++ forkify-app/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '32ecfbff-aa8d-4157-99fa-d53b56433d20';
+export const MODAL_CLOSE_SEC = 2.5;
